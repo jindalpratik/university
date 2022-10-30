1332dfccd841b7402d08276c65bcd624c7cb91e0
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2022-10-30 23:03:48 +0530

    update

--- Courses/Problem Solving using Python Programming/Coding Ninja/9. Arrays & Lists/Find Duplicate.py
+++ Courses/Problem Solving using Python Programming/Coding Ninja/9. Arrays & Lists/Find Duplicate.py
@@ -1,9 +1,4 @@
-# You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
-# Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
-# You need to find and return that number which is unique in the array/list.
-#  Note:
-
-# Unique element is always present in the array/list according to the given condition.
+# You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3 and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.
 
 def find_unique(li_n):
     # print(type(n))
@@ -14,8 +9,9 @@ def find_unique(li_n):
             flag = True
         elif li_n[n] in li_n[(n + 1):]:
             flag = True
-        if not flag:
+        if flag:
             print(li_n[n])
+            break
 run_times = int(input())
 for i in range(run_times):
     n = int(input())
