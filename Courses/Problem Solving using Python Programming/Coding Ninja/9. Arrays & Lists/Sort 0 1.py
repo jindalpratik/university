2b9d2b1bd39a90b557c1af8431ab6b2c0dbd337a
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2022-11-01 20:59:55 +0530

    Update code for sort 0 1

--- Courses/Problem Solving using Python Programming/Coding Ninja/9. Arrays & Lists/Sort 0 1.py
+++ Courses/Problem Solving using Python Programming/Coding Ninja/9. Arrays & Lists/Sort 0 1.py
@@ -2,21 +2,13 @@
 
 from sys import stdin
 
-def sort_and_print(arr, n) :
-    li_0 = []
-    li_1 = []
-    for i in arr:
-        if i == 0:
-            li_0.append(i)
-        elif i == 1:
-            li_1.append(i)
-    # print(li_0)
-    # print(li_1)
-    for x in li_1:
-        li_0.append(x)
-    # print(li_0)
-    for w in range(len(li_0)):
-        print(li_0[w], end = " ")        
+def sortZeroesAndOne(arr,n) :
+    zeroPos = 0
+    for current in range(len(arr)):
+        if arr[current] == 0:
+            arr[current], arr[zeroPos] = arr[zeroPos], arr[current]
+            zeroPos = zeroPos + 1
+    return arr
 
 
 #Taking Input Using Fast I/O
@@ -28,48 +20,17 @@ def takeInput() :
     return arr, n
 
 
+def printList(arr, n) :
+    for i in range(n) :
+        print(arr[i], end = ' ')
+    print()
+
 #main
 t = int(stdin.readline().strip())
 
-while t > 0 :
+while t > 0 : 
     arr, n = takeInput()
-    sort_and_print(arr, n)
+    sortZeroesAndOne(arr, n)
+    printList(arr, n)
     print()
     t -= 1
-
-
-# Another method which exceeds time limit most of the time.
-# from sys import stdin
-
-# def sortZeroesAndOne(arr,n) :
-#     for i in range(len(arr)):
-#             for f in range(i+1,n):
-#                 if arr[i] == 1 and arr[f] == 0:
-#                     arr[i],arr[f] = arr[f],arr[i]
-#                     # print(arr)
-#     return arr
-
-
-# #Taking Input Using Fast I/O
-# def takeInput() :
-#     n = int(stdin.readline().strip())
-#     if n == 0 :
-#         return list(), 0
-#     arr = list(map(int, stdin.readline().strip().split(" ")))
-#     return arr, n
-
-
-# def printList(arr, n) :
-#     for i in range(n) :
-#         print(arr[i], end = ' ')
-#     print()
-
-# #main
-# t = int(stdin.readline().strip())
-
-# while t > 0 : 
-#     arr, n = takeInput()
-#     sortZeroesAndOne(arr, n)
-#     printList(arr, n)
-#     print()
-#     t -= 1
