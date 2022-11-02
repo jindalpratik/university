bab241894e3918b2cd6fdf1ab072af7ad4d0ee57
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2022-11-02 22:37:57 +0530

    2022-11-02
    complete two dimensional lists

--- Courses/Problem Solving using Python Programming/Coding Ninja/10. Two Dimensional Lists/Spiral Print.py
+++ Courses/Problem Solving using Python Programming/Coding Ninja/10. Two Dimensional Lists/Spiral Print.py
@@ -7,53 +7,47 @@
 
 #  Mind that every element will be printed only once.
 
-from sys import stdin
-
 def spiralPrint(mat, nRows, mCols):
     counter = 0
     row_start = 0
     column_start = 0
-    row_end = nRows
-    column_end = mCols
-    while counter <= mCols*nRows:
-        for i in range(column_start, column_end):
-            print("#")
-            print(mat[row_start][i], end = " ")
-            counter += 1
+    row_end = nRows - 1
+    column_end = mCols - 1
+    # print(row_end)
+    # print(column_end)
+    while counter < mCols*nRows:
+        if counter <= mCols*nRows:
+            for i in range(column_start, column_end + 1):
+                print(mat[row_start][i], end = " ")
+                counter += 1
         # print("*")
         row_start += 1
-        for i in range(row_start,row_end):
-            print(mat[i][(column_end-1)], end = " ")
-            counter += 1
+        if counter <= mCols*nRows:
+            for i in range(row_start,row_end + 1):
+                print(mat[i][(column_end)], end = " ")
+                counter += 1
         # print("*")
         column_end -= 1
-        for i in range(column_end - 1, column_start - 1, -1):
-            print(mat[(row_end-1)][i], end = " ")
-            counter += 1
+        if counter < mCols*nRows:
+            for i in range(column_end, column_start - 1, -1):
+                print(mat[(row_end)][i], end = " ")
+                counter += 1
         # print("*")
         row_end -= 1
-        for i in range(row_end -1,row_start -1,-1):
-            print(mat[i][column_start], end = " ")
-            counter += 1
+        if counter < mCols*nRows:
+            for i in range(row_end,row_start -1,-1):
+                print(mat[i][column_start], end = " ")
+                counter += 1
         # print("*")
-        row_end -= 1
+        # row_end -= 1
         column_start += 1     
 
-#Taking Input Using Fast I/O
-def take2DInput() :
-    li = stdin.readline().rstrip().split(" ")
-    nRows = int(li[0])
-    mCols = int(li[1])    
-    if nRows == 0 :
-        return list(), 0, 0    
-    mat = [list(map(int, input().strip().split(" "))) for row in range(nRows)]
-    return mat, nRows, mCols
 
-#main
-t = int(stdin.readline().rstrip())
-
-while t > 0 :
-    mat, nRows, mCols = take2DInput()
+n = int(input())
+for i in range(n):
+    const = input().split()
+    nRows,mCols = int(const[0]),int(const[1])
+    mat = [[int(j) for j in input().split()] for i in range(nRows)]
     spiralPrint(mat, nRows, mCols)
     print()
-    t -= 1
+
