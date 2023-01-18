caaed95ef524c28e3740f89502bd70b31a5869ed
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-01-18 11:39:26 +0530

    change some .ipynb to .py

---
+++ Courses/Problem Solving using Python Programming/Topic-s/Python Lab/2022-11-03/Print all odd numbers..py
@@ -0,0 +1,5 @@
+start = int(input())
+end = int(input())
+for i in range(start, end+1):
+    if not i%2 == 0:
+        print(i)
---
+++ Courses/Problem Solving using Python Programming/Topic-s/Python Lab/2022-11-03/To write a programe to interchange first and last in a list.py
@@ -0,0 +1,4 @@
+li_n = [int(x) for x in input().split()]
+# print(li_n)
+li_n[0],li_n[-1] = li_n[-1],li_n[0]
+print(li_n)
