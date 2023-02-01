4abbf21bd78616ac9852963f2148025f5941cd0f
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-02-01 11:24:44 +0530

    2023-02-01

--- Courses/C_Programming/Theory/2023-02-01/while_loop_3.c
+++ Courses/C_Programming/Theory/2023-02-01/while_loop_3.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int j = 1;
-    while (j += 2, j <= 10)
+    int j = 1 , num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    while (j += 2, j <= num)
     {
         printf("%d ", j);
     }
