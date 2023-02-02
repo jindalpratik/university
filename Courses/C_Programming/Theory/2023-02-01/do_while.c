785f03aca12a84fa63966fd1e820ae2bd749e09c
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-02-02 10:57:19 +0530

    2023-02-02

--- Courses/C_Programming/Theory/2023-02-01/do_while.c
+++ Courses/C_Programming/Theory/2023-02-01/do_while.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 int main()
 {
-    int i = 1;
+    int i = 1, num;
+    printf("Enter the number to print to: ");
+    scanf("%d", &num);
     do
     {
         printf("%d \n", i);
         i++;
-    } while (i <= 10);
+    } while (i <= num);
     return 0;
 }
