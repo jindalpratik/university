9837bc8911b6371e54c049bc37fb9e03652539a1
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-03-22 01:08:45 +0530

    2023-03-22

---
+++ Courses/C_Programming/Theory/2023-03-21/sum_of_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a[] = {11,22,36,5,2};
+    int sum = 0,*p;
+
+    for(p = &a[0]; p <= &a[4]; p++)
+        sum+=*p;
+
+    printf("Sum is %d", sum);
+    return 0;
+}
