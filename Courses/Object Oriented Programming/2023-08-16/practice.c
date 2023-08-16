3b46fdb5d516b9a9b4949060a0e824176e5677d0
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-08-16 17:25:23 +0530

    2023-08-16 OOPS

---
+++ Courses/Object Oriented Programming/2023-08-16/practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+struct dob
+{
+    int day;
+    int month;
+    int year;
+};
+
+struct student
+{
+    char name[20];
+    int roll;
+    struct dob date;
+};
+
+int main()
+{
+    struct student s1;
+    printf("Enter the date of birth :\n");
+    scanf ("%d %d %d", &s1.date.day, &s1.date.month, &s1.date.year);
+    printf("Date of birth is %d/%d/%d", s1.date.day, s1.date.month, s1.date.year);
+    return 0;
+}
