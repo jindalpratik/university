fb271106d70c6c15d8150ec5b7432128de0b2efd
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2023-10-16 20:38:13 +0530

    2023-10-16 OOPS

---
+++ Courses/Object Oriented Programming/2023-10-16/books_1.cpp
@@ -0,0 +1,26 @@
+/*
+To create a book class with the properties being
+title, author, cost and behaviour being open close read bookmark
+*/ 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book{
+    string title;
+    string author;
+    int cost;
+    void open(){
+        cout << "The book is open" << endl;
+    }
+    void close(){
+        cout << "The book is closed" << endl;
+    }
+    void read(){
+        cout << "The book is being read" << endl;
+    }
+    void bookmark(){
+        cout << "The book is bookmarked" << endl;
+    }
+};
